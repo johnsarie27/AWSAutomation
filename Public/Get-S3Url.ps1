8a26553e1a85fd61b07e035c6c84c499d2986a0b
Justin Johns <jjohns@esri.com>
2024-01-31 12:12:32 -0800

    Fix invalid URL issue
    * Fix invalid URL issue
    
    * fix formatting issue

--- Public/Get-S3Url.ps1
+++ Public/Get-S3Url.ps1
@@ -6,10 +6,10 @@ function Get-S3Url {
         Get S3 object URL
     .PARAMETER BucketName
         S3 Bucket name
-    .PARAMETER Region
-        AWS Region
     .PARAMETER Key
         S3 Object key
+    .PARAMETER Region
+        AWS Region
     .INPUTS
         None.
     .OUTPUTS
@@ -20,29 +20,38 @@ function Get-S3Url {
     .NOTES
         Name:     Get-S3Url
         Author:   Justin Johns
-        Version:  0.1.0 | Last Edit: 2023-11-02
-        - 0.1.0 - Initial version
+        Version:  0.1.1 | Last Edit: 2024-01-31
+        - 0.1.1 - (2024-01-31) Fixed issue with incorrect path using region
+        - 0.1.0 - (2023-11-02) Initial version
         Comments: <Comment(s)>
         General notes
     #>
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Mandatory = $true, Position = 0)]
         [ValidateNotNullOrEmpty()]
         [System.String] $BucketName,
 
-        [Parameter(Position = 1)]
-        [ValidateScript({ (Get-AWSRegion).Region -contains $_ })]
-        [System.String] $Region = 'us-east-1',
-
-        [Parameter(Mandatory, Position = 2)]
+        [Parameter(Mandatory = $true, Position = 1)]
         [ValidatePattern('^[\w\./-]+$')]
-        [System.String] $Key
+        [System.String] $Key,
+
+        [Parameter(Mandatory = $false, Position = 2)]
+        [ValidateScript({ (Get-AWSRegion).Region -contains $_ })]
+        [System.String] $Region
     )
     Begin {
         Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
     }
     Process {
-        'https://{0}.s3.{1}.amazonaws.com/{2}' -f $BucketName, $Region, $Key
+        # CHECK FOR REGION
+        if ($PSBoundParameters.ContainsKey('Region')) {
+            # RETURN URL
+            'https://{0}.s3.{1}.amazonaws.com/{2}' -f $BucketName, $Region, $Key
+        }
+        else {
+            # RETURN URL WITHOUT REGION
+            'https://{0}.s3.amazonaws.com/{1}' -f $BucketName, $Key
+        }
     }
 }
