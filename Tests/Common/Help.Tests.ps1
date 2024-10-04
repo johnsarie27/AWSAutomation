48eaae128fe6f655a97f2d706d1dae38125e00c7
Justin Johns <jjohns@esri.com>
2024-10-04 16:24:22 -0700

    Fix validation test errors (#48)

--- Tests/Common/Help.Tests.ps1
+++ Tests/Common/Help.Tests.ps1
@@ -1,7 +1,7 @@
 BeforeDiscovery {
     # Taken with love from @juneb_get_help (https://raw.githubusercontent.com/juneb/PesterTDD/master/Module.Help.Tests.ps1)
     # Import module
-    if (-not (Get-Module -Name $env:BHProjectName -ListAvailable)) {
+    if (-not (Get-Module -Name $env:BHProjectName)) { # -ListAvailable
         Import-Module -Name $env:BHPSModuleManifest -ErrorAction 'Stop' -Force
     }
     $Cmdlets = Get-Command -Module $env:BHProjectName -CommandType 'Cmdlet', 'Function' -ErrorAction 'Stop'
